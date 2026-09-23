17224007ec936ec5b24a95aa090923333fd07d0d
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-23 10:49:13 -0500

    feat(user) implement user registration logic with ducplicare check, bcrypt hashing and a safe dto return

--- src/main/java/com/dentalclinic/webapp/service/UserService.java
+++ src/main/java/com/dentalclinic/webapp/service/UserService.java
@@ -1,9 +1,12 @@
 package com.dentalclinic.webapp.service;
 
+import com.dentalclinic.webapp.dto.request.user.UserRequestDTO;
+import com.dentalclinic.webapp.dto.response.user.UserResponseDTO;
 import com.dentalclinic.webapp.model.User;
 import com.dentalclinic.webapp.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +20,41 @@ public class UserService {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
 
+    }
+
+    // safe dtos to return and get an user
+    public UserResponseDTO userRegistration (UserRequestDTO receiveduserdto){
+        User receivedUser = new User();
+        receivedUser.setFirstnames(receiveduserdto.getFirstnames());
+        receivedUser.setSurname(receiveduserdto.getSurname());
+        receivedUser.setSocialsecuritynumber(receiveduserdto.getSocialsecuritynumber());
+        receivedUser.setEmail(receiveduserdto.getEmail());
+        receivedUser.setPassword(receiveduserdto.getPassword());
+
+        // check if the user already exists
+        Optional queryDbSocialSecNum = userRepository.findBySocialsecuritynumber(receivedUser.getSocialsecuritynumber());
+        Optional queryDbEmail = userRepository.findByEmail(receivedUser.getEmail());
+
+        if(queryDbEmail.isPresent()||queryDbSocialSecNum.isPresent()){
+            throw new RuntimeException("The user already exists");
+        }
+        //this ciphers the password(bcrypt)
+        String plainTextPswd = receivedUser.getPassword();
+        String cipheredPaswd = passwordEncoder.encode(plainTextPswd);
+        //set the safe passwd
+        receivedUser.setPassword(cipheredPaswd);
+        //save
+        userRepository.save(receivedUser);
+        //return the created user by a dto to avoid sensible information
+        UserResponseDTO secureUserReturn = new UserResponseDTO();
+
+        secureUserReturn.setId(receivedUser.getId());
+        secureUserReturn.setFirstnames(receivedUser.getFirstnames());
+        secureUserReturn.setSurname(receivedUser.getSurname());
+        secureUserReturn.setEmail(receivedUser.getEmail());
+        secureUserReturn.setSocialsecuritynumber(receivedUser.getSocialsecuritynumber());
+        secureUserReturn.setRole(receivedUser.getRole());
+
+        return secureUserReturn;
     }
 }
