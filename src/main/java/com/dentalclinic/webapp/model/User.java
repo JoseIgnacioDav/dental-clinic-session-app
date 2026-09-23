350c52b30936ed5fcdebedc560812cde66cd2284
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-23 08:05:20 -0500

    feat(model): User entity with JPA annotations  added also secure read-only role

---
+++ src/main/java/com/dentalclinic/webapp/model/User.java
@@ -0,0 +1,83 @@
+package com.dentalclinic.webapp.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // user data
+    @Column(nullable = false,unique = true)
+    private String socialsecuritynumber;
+    @Column(nullable = false)
+    private String firstnames;
+    @Column(nullable = false)
+    private String Surname;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    // patient set as default
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @Column (nullable = false)
+    private String role = "PATIENT"; // it's really important to keep this protected
+
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getFirstnames() {
+        return firstnames;
+    }
+
+    public void setFirstnames(String firstnames) {
+        this.firstnames = firstnames;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() { // only a getter
+        return role;
+    }
+}
