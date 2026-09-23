bae4206776d69d3fa3b088be480bc2665361d451
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-23 13:01:22 -0500

    feat(controller): create login endpoint with clean DTO and session management

--- src/main/java/com/dentalclinic/webapp/controller/UserController.java
+++ src/main/java/com/dentalclinic/webapp/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.dentalclinic.webapp.controller;
 
+import com.dentalclinic.webapp.dto.request.auth.LoginRequestDTO;
 import com.dentalclinic.webapp.dto.request.user.UserRequestDTO;
 import com.dentalclinic.webapp.dto.response.user.UserResponseDTO;
 import com.dentalclinic.webapp.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,9 +27,13 @@ public class UserController {
         UserResponseDTO newUser = userService.userRegistration(userRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
-
-
-
+    //login endpoint
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDTO> login(@RequestBody LoginRequestDTO credentials, HttpSession session){
+        UserResponseDTO loggedUser = userService.login(credentials);
+        session.setAttribute("loggedUser", loggedUser);
+        return ResponseEntity.ok(loggedUser);
+    }
 
 
 
