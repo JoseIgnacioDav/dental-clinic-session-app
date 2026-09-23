14372f1ccb0387abc789083b6d6e4ed4b6d3d6f1
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-23 09:12:12 -0500

    Creation of UserCreatedDto to return safe information when they register succesfully

---
+++ src/main/java/com/dentalclinic/webapp/dto/response/user/UserCreatedDTO.java
@@ -0,0 +1,61 @@
+package com.dentalclinic.webapp.dto.response.user;
+
+public class UserCreatedDTO {
+    //used to show only this information when the user registers
+    private Long id;
+    private String socialsecuritynumber;
+    private String firstnames;
+    private String surname;
+    private String email;
+    private String role;
+
+    public UserCreatedDTO(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getFirstnames() {
+        return firstnames;
+    }
+
+    public void setFirstnames(String firstnames) {
+        this.firstnames = firstnames;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
