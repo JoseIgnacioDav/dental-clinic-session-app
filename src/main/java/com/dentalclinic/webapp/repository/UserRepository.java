3ca4ad727a4911fcd33b3de8cfbed874b400747d
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-23 08:25:38 -0500

    feat (repo) add UserRepository interface with custom lookup methods to validate users

---
+++ src/main/java/com/dentalclinic/webapp/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.dentalclinic.webapp.repository;
+
+import com.dentalclinic.webapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    //returns optional because User could not exist and we make it safer
+    Optional<User> findByEmail(String email);//
+    //they are used to find if an user already exist in the db
+    Optional<User> findBySocialsecuritynumber(String socialsecuritynumber);
+
+
+    //it will be used to find every user that has a certain role
+    List<User> findByRole(String role);
+
+}
