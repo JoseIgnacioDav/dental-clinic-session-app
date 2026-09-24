1c8536b9addbd1fbb8d4f28dd77d91a49875e210
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-24 05:02:50 -0500

    feat(AppointmentResponse): Create the dto that returns safe information

--- src/main/java/com/dentalclinic/webapp/dto/response/appointment/AppointmentResponse.java
+++ src/main/java/com/dentalclinic/webapp/dto/response/appointment/AppointmentResponse.java
@@ -1,4 +1,63 @@
 package com.dentalclinic.webapp.dto.response.appointment;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AppointmentResponse {
+    private Long id;
+    private LocalDate date;
+    private LocalTime time;
+    private String status;
+    private String doctornames;
+    private String patientnames;
+
+    public AppointmentResponse(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDoctornames() {
+        return doctornames;
+    }
+
+    public void setDoctornames(String doctornames) {
+        this.doctornames = doctornames;
+    }
+
+    public String getPatientnames() {
+        return patientnames;
+    }
+
+    public void setPatientnames(String patientnames) {
+        this.patientnames = patientnames;
+    }
 }
