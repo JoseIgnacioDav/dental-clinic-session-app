bdf400b2b99d3c918344526b343ba57312b9f22c
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-24 05:17:21 -0500

    feat(AppointmentRequestDTO) Configure AppointmentRequestDTO to get safe information

--- src/main/java/com/dentalclinic/webapp/dto/request/appointment/AppointmentRequestDTO.java
+++ src/main/java/com/dentalclinic/webapp/dto/request/appointment/AppointmentRequestDTO.java
@@ -1,4 +1,47 @@
 package com.dentalclinic.webapp.dto.request.appointment;
 
+import com.dentalclinic.webapp.model.User;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AppointmentRequestDTO {
+    private LocalDate date;
+    private LocalTime time;
+    private User doctor;
+    private User patient;
+
+    public AppointmentRequestDTO(){}
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public User getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(User doctor) {
+        this.doctor = doctor;
+    }
+
+    public User getPatient() {
+        return patient;
+    }
+
+    public void setPatient(User patient) {
+        this.patient = patient;
+    }
 }
