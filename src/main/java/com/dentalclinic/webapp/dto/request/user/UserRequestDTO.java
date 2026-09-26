2aeb8d823c8a3817c6827281159c9ea5532e542a
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-26 13:51:24 -0500

    feat(UserRequestDTO & index.html) Detailed validation with messages when they input wrong info

--- src/main/java/com/dentalclinic/webapp/dto/request/user/UserRequestDTO.java
+++ src/main/java/com/dentalclinic/webapp/dto/request/user/UserRequestDTO.java
@@ -2,18 +2,26 @@ package com.dentalclinic.webapp.dto.request.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class UserRequestDTO {
     @NotBlank
+    @Pattern(regexp = "^[0-9]+$", message = "The SSN must contain only numbers")
     private String socialsecuritynumber;
+
     @NotBlank
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", message = "First names must contain only letters")
     private String firstnames;
+
     @NotBlank
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", message = "Surname must contain only letters")
     private String surname;
+
     @NotBlank
     @Email
     private String email;
+
     @NotBlank
     @Size(min = 6,message = "Password must be at least 6 characters")
     private String password;
